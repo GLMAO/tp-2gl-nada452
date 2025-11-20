063c90ca17fee4c9423bb854f8bbb5836bc9ecc6
Nada <nada.bel.twee@gmail.com>
2025-11-20 15:46:38 +0100

    exercice 2

--- src/main/java/com/polytech/App.java
+++ src/main/java/com/polytech/App.java
@@ -1,7 +1,7 @@
 package com.polytech;
 
-import com.polytech.tp.Cours;
-import com.polytech.tp.CoursBuilder;
+import com.polytech.tp.*;
+
 
 /**
  * Hello world!
@@ -11,15 +11,34 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Exercice 1" );
+
         Cours cours = new CoursBuilder()
-                .setMatiere("Mathématiques")
+                .setMatiere("GL")
                 .setEnseignant("Dr. Mazari")
                 .setSalle("inf6")
                 .build();
         System.out.println("cours cree :"+ cours.getDescription());
         System.out.println("Matière : " + cours.getMatiere());
         System.out.println("Enseignant : " + cours.getEnseignant());
+        System.out.println( "Exercice 2" );
+
+        GestionnaireEmploiDuTemps gestionnaire= new GestionnaireEmploiDuTemps();
+        Etudiant etudiant1 = new Etudiant("E1");
+        Etudiant etudiant2 = new Etudiant("E2");
+        Responsable responsable = new Responsable("Mr. Mazri");
+        gestionnaire.attach(etudiant1);
+        gestionnaire.attach(etudiant2);
+        gestionnaire.attach(responsable);
+        System.out.println("\n--- Test ajout de cours ---");
+        gestionnaire.ajouterCours(cours);
+
+        System.out.println("\n--- Test modification de cours ---");
+        gestionnaire.modifierCours(cours, "Changement de salle pour A205");
+
+        System.out.println("\n--- Test changement général ---");
+        gestionnaire.setChangement("Cours annulé pour cause d'une alerte");
+
 
     }
 
--- src/main/java/com/polytech/tp/GestionnaireEmploiDuTemps.java
+++ src/main/java/com/polytech/tp/GestionnaireEmploiDuTemps.java
@@ -3,23 +3,44 @@ package com.polytech.tp;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GestionnaireEmploiDuTemps {
+public class GestionnaireEmploiDuTemps  implements Observer{
     private List<ICours> listeCours = new ArrayList<>();
+    private List<Observer> observers = new ArrayList<>();
+
 
     public void ajouterCours(ICours cours) {
         this.listeCours.add(cours);
         System.out.println("Nouveau cours ajouté : " + cours.getDescription());
         // TODO: C'est ici qu'il faudrait notifier les étudiants (Observer pattern)
+        notifyObservers("nv cours ajouté:" +cours.getDescription());
     }
 
     public void modifierCours(ICours cours, String message) {
         // Logique de modification...
         System.out.println("Cours modifié : " + message);
         // TODO: Notifier les observateurs ici aussi
+        notifyObservers("cours modifie:"+ message);
+
     }
 
-    public void setChangement(String string) {
+    public void setChangement(String message) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setChangement'");
+        notifyObservers("changement dans l'emploi du temps :"+ message);
+    }
+    public void attach( Observer o){
+        observers.add(o);
+    }
+    public void detach(Observer o) {
+        observers.remove(o);
+    }
+    public void notifyObservers(String message) {
+        for (Observer observer : observers) {
+            observer.update(message);
+        }
+    }
+
+    @Override
+    public void update(String message) {
+
     }
 }
